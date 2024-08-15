6ef15149fe7c7f66aed57cb160fd91360c8f5605
Frank Schmidt <frank.schmidt@redspace.com>
2024-08-15 16:07:17 -0300

    add QR code

--- src/App.jsx
+++ src/App.jsx
@@ -70,8 +70,22 @@ function App() {
   return (
     <div>
       <header>
-        <h1>4H Computer Project - Port Hood Island View 4-H Club</h1>
+        <h1>4H Computer Project Showcase</h1>
       </header>
+      {/* Description Card Section */}
+      <div className="description-card">
+        <p>
+          These are the projects created by members of the Port Hood Island View 4H club. The 3D models were designed using
+          <a href="https://www.tinkercad.com" target="_blank" rel="noopener noreferrer"> Tinkercad</a>,
+
+          <a href="https://www.blender.org" target="_blank" rel="noopener noreferrer"> Blender</a>, or
+          <a href="https://www.autodesk.com/products/fusion-360" target="_blank" rel="noopener noreferrer"> Fusion360</a>.
+          The games were created using
+          <a href="https://scratch.mit.edu" target="_blank" rel="noopener noreferrer"> Scratch</a>.
+
+        </p>
+      </div>
+
 
       {/* Section for CAD Models */}
       <h2 className="section-title">CAD Models</h2>
@@ -98,6 +112,15 @@ function App() {
           />
         ))}
       </div>
+
+      <div className="qr-code-section">
+        <img src='/qr.png' alt="QR Code" className="qr-code" />
+        <p className="qr-code-description">
+          You can view this website on your phone by scanning the QR code.
+          If any of the 3D models or Scratch Games don't load here, they might load better on your phone.
+        </p>
+      </div>
+
       <button className="refresh-button" onClick={() => window.location.reload()}>
         Reset
       </button>
