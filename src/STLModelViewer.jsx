973ec1de799d19d72f87f9303e70b39657658424
Frank Schmidt <frank.schmidt@redspace.com>
2024-08-14 23:28:19 -0300

    chore: clean up

--- src/STLModelViewer.jsx
+++ src/STLModelViewer.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import { StlViewer } from "react-stl-viewer";
 
-
-const style = {
-  width: '100%',
-  height: '400px',
-};
-
 const STLModelViewer = ({ stlPath, title, description }) => {
   return (
     <div className="model-container">
@@ -15,11 +9,11 @@ const STLModelViewer = ({ stlPath, title, description }) => {
           url={stlPath}
           width={400}
           height={400}
-          modelColor="#0055ff"
-          backgroundColor="#f0f0f0"
-          rotate={true}
+          modelcolor="#0055ff"
+          backgroundcolor="#f0f0f0"
+          rotate="true"
           orbitControls={true}
-          cameraPosition={{ x: 0, y: 0, z: 100 }}
+          cameraposition={{ x: 0, y: 0, z: 100 }}
         />
       </div>
       <h2>{title}</h2>
